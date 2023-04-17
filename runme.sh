6992147c830b340101acb5dc24b0013d7608a477
TryK <me@tryk.moe>
2023-04-17 12:33:40 +0900

    Update runme.sh

--- runme.sh
+++ runme.sh
@@ -23,12 +23,13 @@ sudo docker network create web-service
 sudo docker-compose up -d
 
 #워드프레스 설치
-#echo "Hello, World" > ./www/index.php
-sudo groupmod -g 82 www-data
-sudo usermod -u 82 www-data
 wget https://ko.wordpress.org/latest-ko_KR.zip -P ./www
 unzip ./www/latest-ko_KR.zip -d ./www
 cp -R ./www/wordpress/* ./www
 rm -rf ./www/wordpress ./www/latest-ko_KR.zip
+
+#권한설정
+sudo groupmod -g 82 www-data
+sudo usermod -u 82 www-data
 sudo chown -R 82:82 www
 sudo chmod 757 www
